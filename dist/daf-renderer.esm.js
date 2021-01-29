6ddbd510eb77161d541b4392899d3a9837997402
Shaun Regenbaum <shaunregenbaum@gmail.com>
2021-01-28 22:43:49 -0500

    fixed + -

--- dist/daf-renderer.esm.js
+++ dist/daf-renderer.esm.js
@@ -88,7 +88,7 @@ function calculateSpacers(mainText, innerText, outerText, options, dummy) {
   };
 
 
-  const topArea = (lineHeight) => ((4 * lineHeight * topWidth) + - paddingAreas.horizontalSide); //remove area of the top 4 lines
+  const topArea = (lineHeight) => ((4 * lineHeight * topWidth) - paddingAreas.horizontalSide); //remove area of the top 4 lines
   
 
   const main = {
