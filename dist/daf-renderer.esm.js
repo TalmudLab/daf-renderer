27c0f41a3b066576c7434e525f889af56ce5aa8f
Dan Jutan <danjutan@gmail.com>
2021-02-01 20:30:11 -0500

    remove debugger statement

--- dist/daf-renderer.esm.js
+++ dist/daf-renderer.esm.js
@@ -433,7 +433,6 @@ function renderer (el, options = defaultOptions) {
       this.spacerHeights = calculateSpacers(main, inner, outer, clonedOptions, containers.dummy);
       styleManager.updateSpacersVars(this.spacerHeights);
       styleManager.manageExceptions(this.spacerHeights);
-      console.log(this.spacerHeights);
       textSpans.main.innerHTML = main;
       textSpans.inner.innerHTML = inner;
       textSpans.outer.innerHTML = outer;
